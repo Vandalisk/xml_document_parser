88a0a9cdc10b0147ba5c30088f6bb72863e832d9
Ilya <zakharuni13@gmail.com>
2016-10-27 19:56:23 +0300

    Add associations to purchase

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,2 +1,5 @@
 class Purchase < ApplicationRecord
+  belongs_to :customer, foreign_key: :customer_id, class_name: "Organization"
+  belongs_to :placer, foreign_key: :placer_id, class_name: "Organization"
+  belongs_to :contact
 end
