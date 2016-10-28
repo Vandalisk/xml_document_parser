f8c1b8a9f1e4a1aa0f97ddc2f785a8d4372f15de
Ilya <zakharuni13@gmail.com>
2016-10-28 13:45:26 +0300

    Add variables to purchase model

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -7,18 +7,13 @@
 #  create_date_time           :datetime
 #  registration_number        :integer
 #  name                       :string
-#  customer_id                :integer
 #  purchase_method_code       :integer
 #  purchase_code_name         :string
-#  placer_id                  :integer
-#  contact                    :integer
 #  publication_date_time      :datetime
 #  modification_date          :datetime
-#  documentation_delivery     :integer
 #  status                     :string
 #  version                    :integer
 #  save_user_id               :integer
-#  nonelectronic_place_info   :integer
 #  examination_place          :text
 #  examination_date_time      :datetime
 #  submission_close_date_time :datetime
@@ -28,7 +23,18 @@
 #
 
 class Purchase < ApplicationRecord
-  belongs_to :customer, foreign_key: :customer_id, class_name: "Organization"
-  belongs_to :placer, foreign_key: :placer_id, class_name: "Organization"
-  belongs_to :contact
+  # ATTRIBUTES = %i(guid create_date_time registration_number name purchase_method_code
+  # purchase_code_name contact publication_date_time modification_date documentation_delivery
+  # status version save_user_id nonelectronic_place_info examination_place examination_date_time
+  # submission_close_date_time publication_planned_date
+  # )
+
+  ASSOCIATED_MODELS = %w(customer placer contact attachments nonelectronic_place_info lots)
+  NOT_INCLUDE = %w(id created_at updated_at)
+
+  has_one :customer, class_name: "Organization"
+  has_one :placer, class_name: "Organization"
+  has_one :contact
+  has_one :documentation_delivery
+  has_one :nonelectronic_place_info
 end
