f2ad8f7b5fe7820e063b4e35ef492a68096899f8
Ilya <zakharuni13@gmail.com>
2016-10-28 13:44:28 +0300

    Add association to lot item with lot

--- db/migrate/20161028063358_create_lot_items.rb
+++ db/migrate/20161028063358_create_lot_items.rb
@@ -6,6 +6,7 @@ class CreateLotItems < ActiveRecord::Migration[5.0]
       t.integer :okdp_id, index: true, foreign_key: true
       t.integer :okved_id, index: true, foreign_key: true
       t.integer :okei_id, index: true, foreign_key: true
+      t.references :lot, index: true, foreign_key: true
       t.column :qty, :bigint
 
       t.timestamps
