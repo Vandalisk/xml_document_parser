14e465b33933f93f387985980906f9476afd04bc
Ilya <zakharuni13@gmail.com>
2016-10-28 13:42:55 +0300

    WIP Add abstract_xml_parser

--- app/services/abstract_xml_parser.rb
+++ app/services/abstract_xml_parser.rb
@@ -4,12 +4,26 @@ class AbstractXmlParser
   end
 
   def parse!
-    doc = Nokogiri::XML(File.open(@file_name))
+    doc = Nokogiri::XML(File.open(@file_path))
 
     namespaces = doc.namespaces
 
-    doc.xpath("//ns2:body//ns2:item//ns2:purchaseNoticeData").each do |item|
+    # TODO: change model determination
+    model = get_model('criteria')
 
+    columns = model.columns_for_parsing
+
+    notice_data = doc.xpath("//ns2:purchaseNoticeData")
+
+    attributes = columns.inject({}) do |hash, column|
+      hash[column.underscore] = notice_data.xpath("//ns2:#{column}").text
+      hash
     end
+
+    new_object = model.new(attributes)
+  end
+
+  def get_model(criteria)
+    Purchase
   end
 end
